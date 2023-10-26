573cb509c4fd24a819c34d9e3bc832069a094c34
Megha Raj <megharajmkdy97@gmail.com>
2023-10-26 17:01:43 +0530

    final commit

--- ecomproject/ecommerceproject/Kyra/urls.py
+++ ecomproject/ecommerceproject/Kyra/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from.import views
-app_name='Kyra'
+from . import views
+
+app_name = 'Kyra'
 urlpatterns = [
-    path('',views.allprodcat,name='allprodcat'),
-    path('<slug:c_slug>/',views.allprodcat,name='products_by_category'),
+    path('', views.allprodcat, name='allprodcat'),
+    path('<slug:c_slug>/', views.allprodcat, name='products_by_category'),
     path('<slug:c_slug>/<slug:product_slug>/', views.prodetail, name='productcatdetail'),
 
 ]
-
